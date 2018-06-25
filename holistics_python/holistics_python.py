77f8b4a4969d829ce73867eabd0bf9923e53e8e9
tamio96 <tanphatvo@yahoo.com>
2018-06-25 14:12:00 +0700

    updating

--- holistics_python/holistics_python.py
+++ holistics_python/holistics_python.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 import requests
 import io
 import json
@@ -6,44 +5,79 @@ import pandas as pd
 import time
 
 class api:
-    def __init__(self, api_key):
-        self.headers = {
+    path = None
+    url = 'https://secure.holistics.io/'
+    headers = {
             "Accept":"application/json", 
             "Content-Type":"application/json",
-            "X-Holistics-Key": api_key}
-    
-    def export_data(self, report_id, filter_dict=dict(), path=None, _page_size=10000000, _page=10000000): 
-        print ("Executing report...")
-        r = requests.get('https://secure.holistics.io/queries/'+report_id+'/submit_export.csv', params = filter_dict, headers = self.headers)
-        if r.status_code != 200:
-            return "Error " + str(r.status_code)
-        print ("Success")
-        job_id = str(r.json()['job_id'])
-        page={
-            'job_id': job_id,
-            '_page_size': _page_size,
-            '_page': _page}
-        print ("Getting export results...")
-        r = requests.get('https://secure.holistics.io/queries/get_export_results.json', params = page,headers = self.headers)
-        if r.status_code != 200:
-            return "Error " + str(r.status_code)
-        print ("Success")
-        res = r.json()
-        while (res['status'] != 'success'):
-            r = requests.get('https://secure.holistics.io/queries/get_export_results.json', params = page,headers = self.headers)
-            res = r.json()
-            time.sleep(1)
-        print ("Downloading export results...")
-        r = requests.get('https://secure.holistics.io/exports/download?job_id=' + job_id,headers = self.headers)
-        if r.status_code != 200:
-            return "Error " + str(r.status_code)
-        text = str(r.content, 'utf-8', errors='replace')    
-        data = pd.read_csv(io.StringIO(text))
-        if path == None:
-            return data
+            "X-Holistics-Key": ''}
+    page={
+            'job_id': '',
+            '_page_size': '10000000',
+            '_page': '10000000'}
+
+    def __init__(self, api_key, path = None, url = None):
+        self.headers['X-Holistics-Key'] = api_key
+        if url is not None:
+            self.url = url
+        if path is not None:
+            self.path = path
+
+    def get_url(self, tail_url, params=None):
+        res = requests.get(self.url + tail_url, params = params, headers = self.headers)
+        if res.status_code != 200:
+            print ("Error " + str(r.status_code))
+            return 0
         else:
-            try:
-                data.to_csv(path, encoding='utf-8', index=False)
-                return "Export to file successed!"
-            except:
-                return "Path not found"
+            return res
+
+    def submit_export(self, report_id, filters=None):
+        print ("Submitting export request... ", end='')
+        tail_url = 'queries/'+report_id+'/submit_export.csv'
+        res = self.get_url(tail_url, filters).json()
+        if res!=0:
+            print ("Success")
+            self.page['job_id'] = str(res['job_id'])
+
+    def get_export_results(self, job_id, _page_size = None, _page = None):
+        print ("Getting export results... ", end='')
+        tail_url = '/queries/get_export_results.json'
+        self.page['job_id']=job_id
+        if _page_size is not None:
+            self.page['_page_size']=_page_size
+        if _page is not None:
+            self.page['_page']=_page
+        res = self.get_url(tail_url, self.page).json()
+        if res!=0:
+            if res['status'] == 'already_existed':
+                self.page['job_id'] = str(res['job_id'])
+            if res['status'] == 'failure':
+                print ("Status: Failure")				
+                return 0
+            while (res['status'] != 'success'):
+                res = self.get_url(tail_url, self.page).json()
+                time.sleep(1)	
+        print ("Success")
+        
+    def download_results(self):
+        print ("Downloading results... ", end='')
+        tail_url = 'exports/download?job_id=' + self.page['job_id']
+        res = self.get_url(tail_url)
+        if res!=0:
+            text = str(res.content, 'utf-8', errors='replace')
+            data = pd.read_csv(io.StringIO(text))
+            print ("Success")
+            if self.path == None:
+                return data
+            else:
+                try:
+                    data.to_csv(path, encoding='utf-8', index=False)
+                    return "Export to file successed!"
+                except:
+                    return "Can't export file to " + self.path
+        
+
+    def export_data(self,report_id,filters=None,_page_size=None,_page=None):
+        self.submit_export(report_id, filters)
+        self.get_export_results(self.page['job_id'],_page_size,_page)
+        return self.download_results()
