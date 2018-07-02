e1fa20600730c729e1145c43dde659830ee76b10
tamio96 <tanphatvo@yahoo.com>
2018-07-02 11:46:29 +0700

    update exception

--- holistics/holistics.py
+++ holistics/holistics.py
@@ -5,48 +5,44 @@ import pandas as pd
 import time
 
 class HolisticsAPI:
-    data = False
-    path = None
-    url = 'https://secure.holistics.io'
-    headers = {
+    def __init__(self, api_key, url = None):
+        self.url = 'https://secure.holistics.io'   
+        self.headers = {
             "Accept":"application/json", 
             "Content-Type":"application/json",
-            "X-Holistics-Key": None}
-    page={
-            'job_id': None,
+            "X-Holistics-Key": 'api_key'}
+        self.page={
+            'job_id': 'job_id',
             '_page_size': '10000000',
             '_page': '10000000'}
-
-    def __init__(self, api_key, path = None, url = None):
         self.headers['X-Holistics-Key'] = api_key
         if url is not None:
             self.url = url
-        if path is not None:
-            self.path = path
-
-    def info(self):
-        return {'api_key': self.headers['X-Holistics-Key'], 'path': self.path, 'url': self.url, }
+        
 
     def get_url(self, tail_url, params=None):
         res = requests.get(self.url + tail_url, params = params, headers = self.headers)
         res.raise_for_status()
         return res
 
-    def submit_report (self, report_id, filters=None):
+    def submit_export (self, report_id, filters=None):
         print ("Submitting export request... ", end='')
         tail_url = '/queries/'+str(report_id)+'/submit_export.csv'
         try:
             res = self.get_url(tail_url, filters)
             res = res.json()
             print ("Success")
-            return str(res['job_id'])
+            self.page['job_id'] = str(res['job_id'])
+            print (str(res['job_id']))
+            return 1
         except requests.exceptions.HTTPError as err:
+            print ("Fail to submit export")
             return err
 
-    def get_export_result(self, job_id, _page_size = None, _page = None):
+    def get_export_results(self, job_id, _page_size = None, _page = None):
         print ("Getting export results... ", end='')
         tail_url = '/queries/get_export_results.json'
-        self.page['job_id']=job_id
+        self.page['job_id']=str(job_id)
         if _page_size is not None:
             self.page['_page_size']=_page_size
         if _page is not None:
@@ -59,48 +55,46 @@ class HolisticsAPI:
                     self.page['job_id'] = str(res['job_id'])
                 if res['status'] == 'failure':
                     print ("Status: Failure")
-                    return 0
+                    raise ValueError('Can\'t get export results')
                 res = self.get_url(tail_url, self.page).json()
                 time.sleep(1)
             print ("Success")
             return 1
         except requests.exceptions.HTTPError as err:
+            print ("Fail to get export results")
             return err
         
-    def download_results(self):
-        print ("Downloading results... ", end='')
-        tail_url = '/exports/download?job_id=' + self.page['job_id']
+    def download_results(self, path):
+        path = str(path)
         try:
+            print ("Downloading results... ", end='')
+            tail_url = '/exports/download?job_id=' + self.page['job_id']        
             res = self.get_url(tail_url)
             text = str(res.content, 'utf-8', errors='replace')
             data = pd.read_csv(io.StringIO(text))
-            self.data = True
-            print ("Success")
-            if self.path == None:
+            if path == None:
                 return data
             else:
                 try:
-                    data.to_csv(self.path, encoding='utf-8', index=False)
+                    data.to_csv(path, encoding='utf-8', index=False)
                     print ("Export to file successed!")
-                    return 1
+                    return data
                 except:
-                    print ("Can't export file to " + self.path)
-        except requests.exceptions.HTTPError as err:
+                    print ("Can't export file to " + path)
+        except (requests.exceptions.HTTPError,pd.errors.ParserError) as err:
+            print ("Fail to download results")
             return err
         
 
-    def export_data(self,report_id,filters=None, page_size=None, page=None):
-        res = self.submit_report(report_id, filters)
-        if type(res) == requests.exceptions.HTTPError:
-            print ("Fail to submit report")
-            return 0
-        res = self.get_export_result(res,page_size,page)
-        if res != 1:
-            print ("Fail to get export results")
-            return 0
-        res = self.download_results()
-        if type(res) == requests.exceptions.HTTPError:
-            print ("Fail to download Results")
+    def export_data(self,report_id, path=None,filters=None, page_size=None, page=None):
+        try:
+            res = self.submit_export(report_id, filters)
+            res = self.get_export_results(self.page['job_id'],page_size,page)
+            res = self.download_results(path)
+            if isinstance(res, ((requests.exceptions.HTTPError, pd.errors.ParserError, ValueError))):
+                raise ValueError
+        except Exception:
+            print ("Fail to export data")
             return 0
         else:
             return res
--- tests/test_holistics.py
+++ tests/test_holistics.py
@@ -1,5 +1,5 @@
 from holistics import HolisticsAPI
-import pandas
+import pandas as pd
 import vcr
 import requests
 
@@ -13,65 +13,66 @@ path = 'output'
 url = 'secure.holistics.io'
 report_id_correct = '33874'
 report_id_wrong = 'aehnaga'
-job_id_correct = '19888412'
+job_id_correct = '19888412'	
 job_id_wrong = 'eargaerg'
 path_correct = 'output.csv'
 path_wrong = 'C:/&^(@#,ga.tacv'
 filters = {'twqdest': 'teqwdst'}
 
 @my_vcr.use_cassette('tests/vcr_cassettes/holistics.yml')
-def test_SubmitReport_correct(): #all correct
+def test_submit_export_correct(): #all correct
 	holistics_instance = HolisticsAPI(api_key_correct)
-	response = holistics_instance.submit_report(report_id_correct, filters)
-	assert type(response) == str, "Response should return string of job_id"
+	response = holistics_instance.submit_export(report_id_correct, filters)
+	assert response == 1, "Response should return string of job_id"
 
 @my_vcr.use_cassette('tests/vcr_cassettes/holistics.yml')
-def test_SubmitReport_wrong():
+def test_submit_export_wrong():
 	holistics_instance = HolisticsAPI(api_key_correct)
-	response = holistics_instance.submit_report(report_id_wrong, filters)
-	assert type(response) == requests.exceptions.HTTPError, "Response should return HTTPError"
+	response = holistics_instance.submit_export(report_id_wrong, filters)
+	assert isinstance(response, requests.exceptions.HTTPError), "Response should return HTTPError"
 
 @my_vcr.use_cassette('tests/vcr_cassettes/holistics_wrongapi.yml')
-def test_SubmitReport_wrong2(): #test with wrong API
+def test_submit_export_wrong2(): #test with wrong API
 	holistics_instance = HolisticsAPI(api_key_wrong)
-	response = holistics_instance.submit_report(report_id_correct, filters)
-	assert type(response) == requests.exceptions.HTTPError, "Response should return HTTPError"
+	response = holistics_instance.submit_export(report_id_correct, filters)
+	assert isinstance(response, requests.exceptions.HTTPError), "Response should return HTTPError"
 
 @my_vcr.use_cassette('tests/vcr_cassettes/holistics.yml')
-def test_GetExportResults_correct(): #all correct
+def test_get_export_results_correct(): #all correct
 	holistics_instance = HolisticsAPI(api_key_correct)
-	response = holistics_instance.get_export_result(job_id_correct, 120, 120)
+	response = holistics_instance.get_export_results(job_id_correct, 120, 120)
 	assert response == 1, "Response must be 1"
 
 @my_vcr.use_cassette('tests/vcr_cassettes/holistics.yml')
-def test_GetExportResults_wrong(): #not-exist job_id
+def test_get_export_results_wrong(): #not-exist job_id
 	holistics_instance = HolisticsAPI(api_key_correct)
-	response = holistics_instance.get_export_result(job_id_wrong, 120, 120)
-	assert response != 1, "Response must be != 1 (0 or HTTPError)"
+	response = holistics_instance.get_export_results(job_id_wrong, 120, 120)
+	assert isinstance(response, (requests.exceptions.HTTPError, ValueError)), "Response must be != 1 (0 or HTTPError)"
 
 @my_vcr.use_cassette('tests/vcr_cassettes/holistics.yml')
-def test_DownloadResults_correct(): #all correct
-	holistics_instance = HolisticsAPI(api_key_correct,path)
+def test_download_results_correct(): #all correct
+	holistics_instance = HolisticsAPI(api_key_correct)
 	holistics_instance.page['job_id'] = job_id_correct
-	response = holistics_instance.download_results()
-	assert type(response) != requests.exceptions.HTTPError, "Response should return dataFrame or 1"
+	response = holistics_instance.download_results(path_correct)
+	assert isinstance(response, pd.DataFrame), "Response should return dataFrame or 1"
 
 @my_vcr.use_cassette('tests/vcr_cassettes/holistics.yml')
-def test_DownloadResults_wrong(): #not-exist job_id
-	holistics_instance = HolisticsAPI(api_key_correct,path=path_wrong)
+def test_download_results_wrong(): #not-exist job_id
+	holistics_instance = HolisticsAPI(api_key_correct)
 	holistics_instance.page['job_id'] = job_id_wrong
-	response = holistics_instance.download_results()
-	assert type(response) == requests.exceptions.HTTPError, "Response should return HTTPError"
+	response = holistics_instance.download_results(path_wrong)
+	assert isinstance(response, (requests.exceptions.HTTPError, pd.errors.ParserError)), "Response should return HTTPError"
 
 @my_vcr.use_cassette('tests/vcr_cassettes/holistics.yml')
-def test_ExportData_correct(): #all correct
+def test_export_data_correct(): #all correct
 	holistics_instance = HolisticsAPI(api_key_correct)
 	response = holistics_instance.export_data(report_id_correct,filters)
-	assert type(response) == int or type(response) == pandas.core.frame.DataFrame, "Response must be != 0"
+	assert isinstance(response, pd.DataFrame), "Response must be != 0"
 
 @my_vcr.use_cassette('tests/vcr_cassettes/holistics.yml')
-def test_ExportData_wrong(): #not-exist report_id
-	holistics_instance = HolisticsAPI(api_key_correct,path=path_correct)
+def test_export_data_wrong(): #not-exist report_id
+	holistics_instance = HolisticsAPI(api_key_correct)
+	print (holistics_instance.page)
 	response = holistics_instance.export_data(report_id_wrong,filters)
 	assert response == 0, "Response must be 0"
 
